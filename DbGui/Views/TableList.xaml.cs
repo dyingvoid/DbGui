05bb65575221bcf9e06f3c750d002a419d5f7d46
dyingvoid <dyingvoid@gmail.com>
2022-12-11 16:03:16 +0500

    Model core added

---
+++ DbGui/Views/TableList.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace DbGui.Views;
+
+public partial class TableListUserControl : UserControl
+{
+    public TableListUserControl()
+    {
+        Table = new()
+        {
+            new() { "I", "am", "watching" },
+            new() { "And", "I", "don't" }
+        };
+        
+        InitializeComponent();
+    }
+    
+    public List<List<string?>> Table { get; set; }
+}
