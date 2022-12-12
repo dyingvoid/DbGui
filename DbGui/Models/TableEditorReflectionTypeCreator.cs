1dce31c2dfa8fd6b37b0665fa92aa6033b7a8420
dyingvoid <dyingvoid@gmail.com>
2022-12-12 22:30:18 +0500

    some refactoring

--- DbGui/Models/TableEditorReflectionTypeCreator.cs
+++ DbGui/Models/TableEditorReflectionTypeCreator.cs
@@ -7,21 +7,6 @@ namespace DbGui.Models;
 
 public static class TableEditorReflectionTypeCreator
 {
-    public static void FillObjectWithData(object? tableObject, 
-        Dictionary<string, Type> csvTableStructure, 
-        List<string?> csvTableStrokeData)
-    {
-        int index = 0;
-        foreach (var (fieldName, fieldType) in csvTableStructure)
-        {
-            tableObject.GetType().InvokeMember(fieldName,
-                BindingFlags.Instance | BindingFlags.Public | BindingFlags.SetProperty,
-                Type.DefaultBinder, tableObject, new object[]{});
-            
-            index++;
-        }
-    }
-
     public static void FillObjectWithDataMetaData(object? tableObject,
         Dictionary<string, Type> csvTableStructure,
         List<string> csvTableStrokeData)
@@ -61,8 +46,7 @@ public static class TableEditorReflectionTypeCreator
         {
             TypeBuilder tb = GetTypeBuilder();
             ConstructorBuilder constructor = tb.DefineDefaultConstructor(MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName);
-
-            // NOTE: assuming your list contains Field objects with fields FieldName(string) and FieldType(Type)
+            
             foreach (var (fieldName, fieldType) in csvTableStructure)
                 CreateProperty(tb, fieldName, fieldType);
 
@@ -73,32 +57,34 @@ public static class TableEditorReflectionTypeCreator
         private static TypeBuilder GetTypeBuilder()
         {
             var typeSignature = "MyDynamicType";
-            var an = new AssemblyName(typeSignature);
-            AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(an, AssemblyBuilderAccess.Run);
-            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
-            TypeBuilder tb = moduleBuilder.DefineType(typeSignature,
-                    TypeAttributes.Public |
-                    TypeAttributes.Class |
-                    TypeAttributes.AutoClass |
-                    TypeAttributes.AnsiClass |
-                    TypeAttributes.BeforeFieldInit |
-                    TypeAttributes.AutoLayout,
+            
+            var assemblyName = new AssemblyName(typeSignature);
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, 
+                AssemblyBuilderAccess.Run);
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName.Name);
+            var typeBuilder = moduleBuilder.DefineType(typeSignature,
+                    TypeAttributes.Public,
                     null);
-            return tb;
+            
+            return typeBuilder;
         }
 
-        private static void CreateProperty(TypeBuilder tb, string propertyName, Type propertyType)
+        private static void CreateProperty(TypeBuilder typeBuilder, string propertyName, Type propertyType)
         {
-            FieldBuilder fieldBuilder = tb.DefineField("_" + propertyName, propertyType, FieldAttributes.Private);
+            FieldBuilder fieldBuilder = typeBuilder.DefineField("_" + propertyName, propertyType, FieldAttributes.Private);
+            PropertyBuilder propertyBuilder = typeBuilder.DefineProperty(propertyName, PropertyAttributes.HasDefault, 
+                propertyType, null);
+            DefineGetterAndSetter(typeBuilder, propertyName, propertyType, fieldBuilder, propertyBuilder);
+        }
 
-            PropertyBuilder propertyBuilder = tb.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null);
+        private static void DefineGetterAndSetter(TypeBuilder tb, string propertyName, Type propertyType,
+            FieldBuilder fieldBuilder, PropertyBuilder propertyBuilder)
+        {
             MethodBuilder getPropMthdBldr = tb.DefineMethod("get_" + propertyName,
-                MethodAttributes.Public | 
-                MethodAttributes.SpecialName | 
-                MethodAttributes.HideBySig, 
-                propertyType, 
+                MethodAttributes.Public,
+                propertyType,
                 Type.EmptyTypes);
-            
+
             ILGenerator getIl = getPropMthdBldr.GetILGenerator();
 
             getIl.Emit(OpCodes.Ldarg_0);
@@ -107,10 +93,10 @@ public static class TableEditorReflectionTypeCreator
 
             MethodBuilder setPropMthdBldr =
                 tb.DefineMethod("set_" + propertyName,
-                  MethodAttributes.Public |
-                  MethodAttributes.SpecialName |
-                  MethodAttributes.HideBySig,
-                  null, new[] { propertyType });
+                    MethodAttributes.Public |
+                    MethodAttributes.SpecialName |
+                    MethodAttributes.HideBySig,
+                    null, new[] { propertyType });
 
             ILGenerator setIl = setPropMthdBldr.GetILGenerator();
             Label modifyProperty = setIl.DefineLabel();
